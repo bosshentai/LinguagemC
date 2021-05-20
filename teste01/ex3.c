2ec486d3e04aace73f979d8c1169ea290d90f1fc
bosshentai <baptistamhernani@gmail.com>
2021-05-20 09:12:38 -0100

    fixed ex3.c

--- teste01/ex3.c
+++ teste01/ex3.c
@@ -8,13 +8,13 @@ float delta(float a, float b, float c)
     return pow(b, 2) - 4 * a * c;
 }
 
-void bhashara(float delta,float a, float b){
-    float x1,x2;
-    x1 = (float)( - b + sqrt(delta) )/ 2 * a;
-    x2 = ( - b - sqrt(delta) )/ 2 * a;
+void bhashara(float delta,float a, float b,float *x1, float *x2){
+    //float x1,x2;
+    *x1 = (float)( - b + sqrt(delta) )/ 2 * a;
+    *x2 = ( - b - sqrt(delta) )/ 2 * a;
 
-    printf("x1 = %.2f \n",x1);
-    printf("x2 = %.2f \n",x2);
+    // printf("x1 = %.2f \n",x1);
+    // printf("x2 = %.2f \n",x2);
 }
 
 void main()
@@ -23,9 +23,14 @@ void main()
     float a = 4;
     float b = 30;
     float c = 4;
+    float x1;
+    float x2;
 
     float resultDelta = delta(a, b, c);
     // exer 3 pa termina qel traboi x1 e x2 
 
-   bhashara(resultDelta,a,b);
+   bhashara(resultDelta,a,b,&x1,&x2);
+
+   printf("x1 = %.2f \n",x1);
+   printf("x2 = %.2f \n",x2);
 }
